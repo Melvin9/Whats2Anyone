ecbbc1fe492e25e93057463210b08cb2a6b6ce00
Melvin <melvinmathew0102@gmail.com>
2018-07-28 15:40:08 +0530

    Save Status Completed

--- app/src/main/java/com/melvin9/whatsapp/direct/message/whatsanyone/Messages_Adapter.java
+++ app/src/main/java/com/melvin9/whatsapp/direct/message/whatsanyone/Messages_Adapter.java
@@ -10,8 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.melvin9.whatsapp.direct.message.whatsanyone.R;
-
 import java.util.List;
 
 /**
